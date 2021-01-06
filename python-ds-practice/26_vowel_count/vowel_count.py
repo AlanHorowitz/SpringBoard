3f2fbd0511780b647cb1699c2b284669a5707081
Alan Horowitz <alanhorowitz63@gmail.com>
2021-01-06 13:38:22 -0500

    test commit from VS code

--- python-ds-practice/26_vowel_count/vowel_count.py
+++ python-ds-practice/26_vowel_count/vowel_count.py
@@ -7,6 +7,7 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    # Add a comment to practice committing from VS Code
     low_phrase = phrase.lower()
     d = {}
     for l in low_phrase:
