4597a44bc7160c7ddd96188da5662a7241d7b3ab
Alan Horowitz <alanhorowitz63@gmail.com>
2021-02-04 14:29:25 -0500

    more hacking

--- python-oop-mini-project/banking.py
+++ python-oop-mini-project/banking.py
@@ -1,193 +1,178 @@
 from customer import Customer, CustomerAlreadyExistsException
 from account import Account, AccountTransactionAbortedException
+# from bankutils import select_from_menu
 
+def select_from_menu(header, question, options):
+
+    print('\033c', end='')  # clear screen
+    print('-' * len(header))
+    print(header)
+    print('-' * len(header))
+    print('\n' + question + '\n')
+    for i, option in enumerate(options):
+        print("[" + str(i+1) + "] " + option[0])
+
+    while True:
+        print("\nPlease select an option from menu." , end='')
+        s = input().strip()
+        if s.isnumeric() and int(s) in range(1,len(options)+1):
+            return options[int(s)-1][1]
+            
 class Bank():
 
+    OPEN_NEW_ACCOUNT = "OPEN_NEW_ACCOUNT"
+    RUN_TRANSACTION = "RUN_TRANSACTION"
+    QUIT = "QUIT"
+    YES = "YES"
+    NO = "NO"
+
     customers = {}
-    accounts = {}
-    next_customer_id = 1
-    next_account_id = 1
+    accounts = {}    
 
     class AccountValidationException(Exception):
         pass
     class InvalidCredentialsException(Exception):
         pass
 
-    def welcome_screen(self):
-        print("-----------------------------------------")
-        print("     Welcome to Simple Friendly Bank     ")
-        print("-----------------------------------------")
-        print("")
-        print("What would you like to do?")
-        print("")
-        print("[1] Open a new account" )
-        print("[2] Access an existing account")
-        print("[3] Quit")
-        print("")
+    def persist(self):
+        pass
 
-        while True:
-            print("Please Enter from menu. Use [3] to quit: ", end='')
-            s = input().strip()
-            if s == "1":
-                return self.new_account_screen 
-            elif s == "2":
-                return self.existing_account_screen
-            elif s == "3":
-                return None
-                
-    def new_account_screen(self):
-        print("-----------------------------------------")
-        print("         Open a new account              ")
-        print("-----------------------------------------")
-        print("")
-        print("Are you an existing customer?")
-        print("")
-        print("[1] Yes" )
-        print("[2] No")
-        print("[3] Quit")
-        print("")
+    def welcome_user(self):
 
-        s = ""
-        while s != "3":
+        header = "     Welcome to Simple Friendly Bank     "
+        question = "What would you like to do?"
+        options = (("Open a new account", Bank.OPEN_NEW_ACCOUNT),
+                    ("Access an existing account", Bank.RUN_TRANSACTION),
+                    ("Quit", Bank.QUIT))
 
-            print("Please Enter from menu. Use [3] to quit: ", end='')
-            s = input().strip()
+        return select_from_menu(header, question, options)
 
-            if s == "1":
+    def get_new_account_type():
 
-                print("\nOK, Let's create your new account")
-                customer_id = input("\nPlease enter your customer id")
-                customer_pin = input("\nPlease enter your pin")
-                try:
-                    cust = self.validate_customer_credentials(customer_id, customer_pin)
-                    acct = Account.create_new_account(cust, self.next_account_id)
-                    print("\nCongratulations, Your new {type} account has been created! Your account id is {id}.".format(n=acct.customer_id)) 
-                    print("\nPlease remember this id and your pin for future transactions.")
-                    next_screen = self.welcome_screen
-                except(self.InvalidCredentialsException):
-                    print("\nPin is not valid for customer id {n}. ".format(n=cust.customer_id))
-                    next_screen = self.new_account_screen 
-                    pass
-                except(AccountTransactionAbortedException):
-                    pass
+        header = ""
+        question = "Select the desired type of account"
+        options = (("Checking Account", Account.CHECKING),
+                   ("Savings Account", Account.SAVINGS))
 
-
-                input("\n\nPress enter to continue ")
-                return next_screen
-
-            elif s == "2":
-
-                try:  
-                    cust = self.create_new_customer()  
-                    print("\nCongratulations, You are our new customer! Your customer id is: {n}.".format(n=cust.customer_id)) 
-                    print("\nPlease remember this id and your pin for future transactions.")
-                    next_screen = self.welcome_screen
-
-                except(CustomerAlreadyExistsException):
-                    print("\nCustomer was a duplicate and could not be added.")
-                    next_screen = self.new_account_screen
-                    
-                input("\n\nPress enter to continue ")
-                return next_screen
+        return select_from_menu(header, question, options)
             
-            elif s == "3":
-                return self.welcome_screen
+    def get_transaction_method(transaction_metadata):
 
-    def existing_account_screen(self):
-        print("-----------------------------------------")
-        print("         Access existing account         ")
-        print("-----------------------------------------")
-        print("")
-        print("Are you an existing customer?")
-        print("")
-        print("[1] Yes" )
-        print("[2] No")
-        print("[3] Quit")
-        print("")
-        try:
-            acct = self.get_validated_account()
-            option = acct.get_options()
-            transaction_data = option.get_transaction_data()
-            acct.run_transaction(option, transaction_data)
+        header = ""
+        question = "Available transactions"
+        options = [(transaction_metadata[meta][0], meta) for meta in transaction_metadata]  # (display_name, method)
 
-        except(self.AccountValidationError):
-            print("bad account number or id")
-
-        except(self.TransactionFailedError):
-            print("bad account number or id")
-
-        except(AccountTransactionAbortedException):
-            pass
-
-
-        return self.welcome_screen
-
-    # replacement routime --  Open Checking or savings account by interacting with the metadata
-    # prompt user for data
-    # consider skipping defaulted parms.  Account validates.
-    def open_account(self, account):
+        return select_from_menu(header, question, options)
+        
     
+    def open_new_account(self):
+
+        customer = self.get_validated_customer()  
+        type = self.get_new_account_type()
+        if type == Account.CHECKING:
+            account = CheckingAccount(customer.getId())
+        if type == Account.SAVINGS:
+            account = SavingsAccount(customer.getId())
         open_metadata = account.get_open_metadata()
         open_metadata_parameters = open_metadata['open'][1]
         kwargs = {parm : input(text) for parm, text in open_metadata_parameters}
         account.open(**kwargs)
 
+        accounts[account.getID()] = account
+        customer.add_account(account.getId())
+        self.persist()
+        input("Congratulations. You're account has been opened  Please hit return to continue")
+  
+    def run_transaction(self):
+
+        account = self.get_validated_account()    
+        customer = customers[account.get_custId()]
+        print("Hi, " + {0} + 'Welcome Back!'.format(customers.get_customer_id()))  
+        transaction_metadata = account.get_transaction_metadata()
+        transaction_method  = self.get_transaction_method(transaction_metadata)  # pick desired function from menu
+        transaction_parameters = transaction_metadata[transaction_method][1]
+        kwargs = {parm : input(text) for parm, text in transaction_parameters}
+        getattr(account, transaction_method)(**kwargs)                           # call method
+
+        input("Congratulations. Transaction completed successfully.  Please hit return to continue")
+
     def run(self):
 
-        next = self.welcome_screen    
-        # process screens until done
-        while next:
-            print('\033c', end='')  # clean screen
-            next = next()
-        print("\nThank you for visiting us.  Have a great day!")
+        while True:
+            try:
+                activity = welcome_user()
+                if activity == Bank.RUN_TRANSACTION:
+                    self.run_transaction()
+                elif activity == Bank.OPEN_NEW_ACCOUNT:
+                    self.open_new_account()
+                elif activity == Bank.QUIT:
+                    print("\nThank you for visiting us.  Have a great day!")
+                    break
+                else:
+                    print("\nSystem Error")
+                    break
+            # handle all exceptions  here
+            except(Exception):
+                input("error message.  Please hit return to continue")
 
-    def has_customer(self, first_name, last_name, ssn):
-        return False
+    def get_validated_customer(self):
 
-    def create_new_customer(self):
-        '''
-        Interact with the user to collect customer infomation.  Raise a CustomerAlreadyExists exception, if there is a customer
-        with the same first_name, last_name and ssn.
-        '''
-        cust = Customer(self.next_customer_id)
-        
-        cust.get_validation_parameters()
-        if cust in self.customers.values():    # customer equality based on valuation parameters
-            raise CustomerAlreadyExistsException
-        cust.get_other_parameters()
-        assert(self.customers.get(cust.customer_id) == None)
-        self.customers[cust.customer_id] = cust
-        self.next_customer_id += 1
+        header =   "         Open a new account              "
+        question = "Are you an existing customer?"
+        options = (("Yes", Bank.YES),
+                    ("No", Bank.NO))
 
-        return cust
+        option = select_from_menu(header, question, options)            
+        if option == Bank.YES:
+            print("")
+            customer_id = input("Please enter your customer id: ").strip()
+            pin = input("Please enter your pin: ").strip()
+            customer = self.customers.get(customer_id)
+            if customer == None or customer.pin != pin:
+                raise self.InvalidCredentialsException 
+            return customer
+        elif option == Bank.NO:               
+            return self.get_new_customer()
+        else:
+            raise BankSystemError
 
+    def get_new_customer(self):
+            '''
+            Interact with the user to collect necessary infomation.  Return the new customer or raise an exception if the customer already exists
+            or there are validation errors.
+            '''
+            customer = Customer()
+
+            print("")
+            print("OK.  Let's sign you up with the following information") 
+
+            info_metadata = customer.get_info_metadata()
+            info_metadata_parameters = info_metadata['set_info'][1]
+            kwargs = {parm : input(text) for parm, text in info_metadata_parameters}
+            customer.set_info(**kwargs)            
+                        
+            if cust in self.customers.values():    # customer equality based on first_name, last_name ssn
+                raise CustomerAlreadyExistsException
+            else:
+                self.customers[customer.getId()] = customer    # add to working dictionary    
+                print("\nCongratulations, You are our new customer! Your customer id is: {n}.".format(n=cust.customer_id)) 
+                print("\nPlease remember this id and your pin for future transactions.")
+                input("Please hit return to continue")
+                return customer
+
+    
     def get_validated_account(self):
         
+        # look for id in accounts dictionary and then check pin
         account_id =  input("\Please enter your account id ").strip()     
         pin        =  input("\nPlease enter your PIN ").strip()
-        custs = [cust for cust in self.customers.values() 
-                     if account_id in cust.accounts and cust.pin == pin]
-        if len(custs) != 1:
-            raise(self.AccountValidationException)
-        cust = custs[0]
-        acct = Account.create_new_account(custs[0].id, self.next_account_id)
-        cust.add_account(acct)
-        self.accounts[acct.account_id] = acct
-        self.next_account_id += 1
-        return acct
-        
 
-    def validate_customer_credentials(self, customer_id, customer_pin):
-        '''
-        return validated Customer or raise InvalidCredentialsException
-        '''
-        cust = self.customers.get(customer_id)
-        if cust == None or cust.customer_pin != customer_pin.strip():
-            raise self.InvalidCredentialsException 
-        return cust
+        accounts.get(account_id)
+        if account:            
+            customer = customers[account.get_customerId()]
+            if pin == customer.getPin():      
+                return account
+
+        raise(InvalidCredentialsException)
+
     
-    def validate_account_credentials(self, account_id, customer_pin):
-        pass
-
-    def persist_customers(self):
-        pass
