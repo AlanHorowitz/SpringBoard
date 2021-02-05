58f117573229d88427c8a42b165f12c73a09b042
Alan Horowitz <alanhorowitz63@gmail.com>
2021-02-04 21:59:00 -0500

    getting there

--- python-oop-mini-project/banking.py
+++ python-oop-mini-project/banking.py
@@ -2,6 +2,7 @@ from customer import Customer, CustomerAlreadyExistsException
 from account import Account, AccountTransactionAbortedException
 # from bankutils import select_from_menu
 
+
 def select_from_menu(header, question, options):
 
     print('\033c', end='')  # clear screen
@@ -13,11 +14,12 @@ def select_from_menu(header, question, options):
         print("[" + str(i+1) + "] " + option[0])
 
     while True:
-        print("\nPlease select an option from menu." , end='')
+        print("\nPlease select an option from menu.", end='')
         s = input().strip()
-        if s.isnumeric() and int(s) in range(1,len(options)+1):
+        if s.isnumeric() and int(s) in range(1, len(options)+1):
             return options[int(s)-1][1]
-            
+
+
 class Bank():
 
     OPEN_NEW_ACCOUNT = "OPEN_NEW_ACCOUNT"
@@ -27,10 +29,11 @@ class Bank():
     NO = "NO"
 
     customers = {}
-    accounts = {}    
+    accounts = {}
 
     class AccountValidationException(Exception):
         pass
+
     class InvalidCredentialsException(Exception):
         pass
 
@@ -42,12 +45,12 @@ class Bank():
         header = "     Welcome to Simple Friendly Bank     "
         question = "What would you like to do?"
         options = (("Open a new account", Bank.OPEN_NEW_ACCOUNT),
-                    ("Access an existing account", Bank.RUN_TRANSACTION),
-                    ("Quit", Bank.QUIT))
+                   ("Access an existing account", Bank.RUN_TRANSACTION),
+                   ("Quit", Bank.QUIT))
 
         return select_from_menu(header, question, options)
 
-    def get_new_account_type():
+    def get_new_account_type(self):
 
         header = ""
         question = "Select the desired type of account"
@@ -55,19 +58,19 @@ class Bank():
                    ("Savings Account", Account.SAVINGS))
 
         return select_from_menu(header, question, options)
-            
+
     def get_transaction_method(transaction_metadata):
 
         header = ""
         question = "Available transactions"
-        options = [(transaction_metadata[meta][0], meta) for meta in transaction_metadata]  # (display_name, method)
+        options = [(transaction_metadata[meta][0], meta)
+                   for meta in transaction_metadata]  # (display_name, method)
 
         return select_from_menu(header, question, options)
-        
-    
+
     def open_new_account(self):
 
-        customer = self.get_validated_customer()  
+        customer = self.get_validated_customer()
         type = self.get_new_account_type()
         if type == Account.CHECKING:
             account = CheckingAccount(customer.getId())
@@ -75,24 +78,28 @@ class Bank():
             account = SavingsAccount(customer.getId())
         open_metadata = account.get_open_metadata()
         open_metadata_parameters = open_metadata['open'][1]
-        kwargs = {parm : input(text) for parm, text in open_metadata_parameters}
+        kwargs = {parm: input(text) for parm, text in open_metadata_parameters}
         account.open(**kwargs)
 
-        accounts[account.getID()] = account
+        self.accounts[account.getID()] = account
         customer.add_account(account.getId())
         self.persist()
-        input("Congratulations. You're account has been opened  Please hit return to continue")
-  
+        input(
+            "Congratulations. You're account has been opened  Please hit return to continue")
+
     def run_transaction(self):
 
-        account = self.get_validated_account()    
-        customer = customers[account.get_custId()]
-        print("Hi, " + {0} + 'Welcome Back!'.format(customers.get_customer_id()))  
+        account = self.get_validated_account()
+        customer = self.customers[account.get_custId()]
+        print("Hi, " + {0} +
+              'Welcome Back!'.format(customers.get_customer_id()))
         transaction_metadata = account.get_transaction_metadata()
-        transaction_method  = self.get_transaction_method(transaction_metadata)  # pick desired function from menu
+        transaction_method = self.get_transaction_method(
+            transaction_metadata)  # pick desired function from menu
         transaction_parameters = transaction_metadata[transaction_method][1]
-        kwargs = {parm : input(text) for parm, text in transaction_parameters}
-        getattr(account, transaction_method)(**kwargs)                           # call method
+        kwargs = {parm: input(text) for parm, text in transaction_parameters}
+        getattr(account, transaction_method)(
+            **kwargs)                           # call method
 
         input("Congratulations. Transaction completed successfully.  Please hit return to continue")
 
@@ -100,7 +107,7 @@ class Bank():
 
         while True:
             try:
-                activity = welcome_user()
+                activity = self.welcome_user()
                 if activity == Bank.RUN_TRANSACTION:
                     self.run_transaction()
                 elif activity == Bank.OPEN_NEW_ACCOUNT:
@@ -117,62 +124,61 @@ class Bank():
 
     def get_validated_customer(self):
 
-        header =   "         Open a new account              "
+        header = "         Open a new account              "
         question = "Are you an existing customer?"
         options = (("Yes", Bank.YES),
-                    ("No", Bank.NO))
+                   ("No", Bank.NO))
 
-        option = select_from_menu(header, question, options)            
+        option = select_from_menu(header, question, options)
         if option == Bank.YES:
             print("")
             customer_id = input("Please enter your customer id: ").strip()
             pin = input("Please enter your pin: ").strip()
             customer = self.customers.get(customer_id)
             if customer == None or customer.pin != pin:
-                raise self.InvalidCredentialsException 
+                raise self.InvalidCredentialsException
             return customer
-        elif option == Bank.NO:               
+        elif option == Bank.NO:
             return self.get_new_customer()
         else:
             raise BankSystemError
 
     def get_new_customer(self):
-            '''
-            Interact with the user to collect necessary infomation.  Return the new customer or raise an exception if the customer already exists
-            or there are validation errors.
-            '''
-            customer = Customer()
+        '''
+        Interact with the user to collect necessary infomation.  Return the new customer or raise an exception if the customer already exists
+        or there are validation errors.
+        '''
+        customer = Customer()
 
-            print("")
-            print("OK.  Let's sign you up with the following information") 
+        print("")
+        print("OK.  Let's sign you up with the following information")
 
-            info_metadata = customer.get_info_metadata()
-            info_metadata_parameters = info_metadata['set_info'][1]
-            kwargs = {parm : input(text) for parm, text in info_metadata_parameters}
-            customer.set_info(**kwargs)            
-                        
-            if cust in self.customers.values():    # customer equality based on first_name, last_name ssn
-                raise CustomerAlreadyExistsException
-            else:
-                self.customers[customer.getId()] = customer    # add to working dictionary    
-                print("\nCongratulations, You are our new customer! Your customer id is: {n}.".format(n=cust.customer_id)) 
-                print("\nPlease remember this id and your pin for future transactions.")
-                input("Please hit return to continue")
-                return customer
+        info_metadata = customer.get_info_metadata()
+        info_metadata_parameters = info_metadata['set_info'][1]
+        kwargs = {parm: input(text) for parm, text in info_metadata_parameters}
+        customer.set_info(**kwargs)
 
-    
+        if cust in self.customers.values():    # customer equality based on first_name, last_name ssn
+            raise CustomerAlreadyExistsException
+        else:
+            # add to working dictionary
+            self.customers[customer.id)] = customer
+            print("\nCongratulations, You are our new customer! Your customer id is: {n}.".format(
+                n=cust.customer_id))
+            print("\nPlease remember this id and your pin for future transactions.")
+            input("Please hit return to continue")
+            return customer
+
     def get_validated_account(self):
-        
+
         # look for id in accounts dictionary and then check pin
-        account_id =  input("\Please enter your account id ").strip()     
-        pin        =  input("\nPlease enter your PIN ").strip()
+        account_id = input("\Please enter your account id ").strip()
+        pin = input("\nPlease enter your PIN ").strip()
 
         accounts.get(account_id)
-        if account:            
+        if account:
             customer = customers[account.get_customerId()]
-            if pin == customer.getPin():      
+            if pin == customer.getPin():
                 return account
 
         raise(InvalidCredentialsException)
-
-    
--- python-oop-mini-project/customer.py
+++ python-oop-mini-project/customer.py
@@ -4,35 +4,60 @@ class CustomerAlreadyExistsException(Exception):
 class Customer():
 
     CUSTOMER_ID_PREFIX = "CUST"
+    next_customer_id = 1
 
-    def __init__(self, id):
-        self.customer_id = Customer.CUSTOMER_ID_PREFIX + str(id)
-        self.accounts = []
+    def __init__(self):
+
+        self._id = Customer.CUSTOMER_ID_PREFIX + str(Customer.next_customer_id)
+        self._accounts = []
+
+        self._first_name = ""
+        self._last_name = ""
+        self._social_security_number = ""
+        self._street_address = ""
+        self._state = ""
+        self._zip_code = ""
+        self._pin = "" 
+
+        Customer.next_customer_id += 1
+
+    def get_info_metadata(self):
+
+        return {'set_info': ('No Display',[('first_name', 'Please enter your first name:'),
+                                             ('last_name', 'Please enter your last name:'),
+                                             ('social_security_number', 'Please enter your social security number'),   
+                                            ('pin','Please enter your personal identification number' )])}
+        
+    def set_info(self, **kwargs):
+
+        self._first_name = kwargs.get('first_name')
+        self._last_name = kwargs.get('last_name')
+        self._social_security_number = kwargs.get('social_security_number')
+        self._pin = kwargs.get('pin')
+
+    @property
+    def id(self):   
+        return self._id
+
+    @property 
+    def first_name(self):
+        return self._first_name
+
+    @property 
+    def last_name(self):
+        return self._last_name
+
+    @property
+    def social_security_number(self):
+        return self._social_security_number
+
+    def add_account(self, account_id):
+        self._accounts.append(account_id)
 
     def __eq__(self, other):
         if isinstance(other, Customer):
             if ((self.first_name == other.first_name) and 
                 (self.last_name == other.last_name) and 
-                (self.ssn == other.ssn)):
+                (self.social_security_number == other.social_security_number)):
                     return True
-        return False
-             
-    def add_account(self, account):
-        self.accounts.append(account.account_id)
-
-    def load_customer(self):
-        '''
-        Retrieve customer from persistence 
-        '''
-        pass
-
-    def get_validation_parameters(self):
-        self.first_name = input("\nYour first name            : ") 
-        self.last_name =  input("\nYour last name             : ")     
-        self.ssn =        input("\nYour social security number: ")   
-
-    def get_other_parameters(self):
-        self.street_address = input("\nYour street address        : ") 
-        self.state =          input("\nYour state                 : ")     
-        self.zip_code=        input("\nYour zip code              : ")
-        self.pin =            input("\n\nNow, choose a personal identification number: ")
+        return False            
