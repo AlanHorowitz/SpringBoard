f6f32edc10893cdf8df229127f865684aae0c0ed
Alan Horowitz <alanhorowitz63@gmail.com>
2021-02-07 17:01:37 -0500

    delete unneeded files

--- python-oop-mini-project/banking_test.py
+++
@@ -1,8 +0,0 @@
-from banking import Bank
-from banking import Customer
-from banking import CustomerAlreadyExistsException
-
-def test_1():
-    cust = Customer()
-    cust.get_validation_parameters()
-    print(cust)
