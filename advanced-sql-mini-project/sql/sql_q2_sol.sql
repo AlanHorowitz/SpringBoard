ef38c0dd205e502048f028e05d91a268de3a050b
Alan Horowitz <alanhorowitz63@gmail.com>
2021-03-13 18:15:59 -0500

    removing sql project to get clean squach commit on branch

--- advanced-sql-mini-project/sql/sql_q2_sol.sql
+++
@@ -1,6 +0,0 @@
-use euro_cup_2016;
-
--- Write a SQL query to find the number of matches that were won by penalty shootout.
-
-select count(*) from match_mast
-where decided_by = 'P';
--- advanced-sql-mini-project/sql/sql_q5_sol.sql
+++
@@ -1,6 +0,0 @@
-use euro_cup_2016;
-
--- Write a SQL query to find the number of bookings that happened in stoppage time.
-
-select count(*) from player_booked
-where play_schedule = 'ST';
