d9d0560e0ed72ce7f79dab003de85025b16c07a8
Alan Horowitz <alanhorowitz63@gmail.com>
2021-03-16 19:18:22 -0400

    update before black

--- open-ended-capstone/README.md
+++ open-ended-capstone/README.md
@@ -1,13 +1,13 @@
-##  second_etl.py
+##  RetailDW Module
 
-###  This program uses psycopg2 to load and update the Product table of the proposed Postgres ETL source system.  Initial load is of 5000 records and which is followed by five incremental loads, each with 200 inserts and 50 updates.  It may be run repeatedly.
+###  This initial version uses psycopg2 to load and update the Product table of the proposed Postgres ETL source system.  Initial load is of 5000 records and which is followed by five incremental loads, each with 200 inserts and 50 updates.  It may be run repeatedly.
 
 ###  Issue the following commands from the open-ended-capstone directory to build and run:
 
 ```
 docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
-docker build -t second_etl .
-docker run second_etl
+docker build -t retail_dw .
+docker run retail_dw
 ```
 ###   Expected output:
 
--- open-ended-capstone/RetailDW/sqltypes.py
+++ open-ended-capstone/RetailDW/sqltypes.py
@@ -1,31 +1,39 @@
 import random
+from typing import List
 
 class Column():
+    """Database Column metadata"""
 
-    def __init__(self, column_name, column_type, isPrimaryKey=False):
+    def __init__(self, column_name : str, column_type : str, isPrimaryKey : bool=False):
 
         self._name = column_name
         self._type = column_type
         self._isPrimaryKey = isPrimaryKey
 
-    def get_name(self):
+    def get_name(self) -> str:
 
         return self._name
 
-    def get_type(self):
+    def get_type(self) -> str:
 
         return self._type
 
-    def isPrimaryKey(self):
+    def isPrimaryKey(self) -> bool:
 
         return self._isPrimaryKey
 
 class Table():
+    """Database Table metadata"""
 
-    def __init__(self, name, *columns):
+    def __init__(self, name : str, *columns : Column):
+        """Instatiate a table metadata object.
+
+        Note: Source system tables in RetailDW must have a single column integer primary key
+        and at least one VARCHAR column.
+        """
 
         self._name = name
-        self._columns= [ col for col in columns ] 
+        self._columns = [ col for col in columns ] 
         primary_keys = [ col.get_name() for col in columns if col.isPrimaryKey() ] 
         if len(primary_keys) != 1:
             raise Exception("Simulator requires exactly one primary key")
@@ -34,24 +42,25 @@ class Table():
         if len(primary_keys) == 0:
             raise Exception("Need at least one VARCHAR for update")
 
-    def get_name(self):
+    def get_name(self) -> str:
 
         return self._name
 
-    def get_columns(self):
+    def get_columns(self) -> List[Column]:
+        """ Return a complete list of Column objects for the table."""
 
         return self._columns
 
-    def get_primary_key(self):
+    def get_primary_key(self) -> str:
 
         return self._primary_key
 
-    def get_column_names(self):
-
+    def get_column_names(self) -> List[str]:
+        """ Return a complete list of Column names for the table."""
         return [col.get_name() for col in self._columns] 
 
-    def get_update_column(self):
-
+    def get_update_column(self) -> Column:
+        """ Return a random eligible update column."""
         i = random.randint(0,len(self._update_columns)-1)   
         return self._update_columns[i]
 
