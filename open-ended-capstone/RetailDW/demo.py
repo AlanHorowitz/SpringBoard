0c445f48608c94f9b12551130becd84411495a8c
Alan Horowitz <alanhorowitz63@gmail.com>
2021-03-31 12:43:11 -0400

    demo1 -> demo2 single commit

--- open-ended-capstone/RetailDW/demo.py
+++ open-ended-capstone/RetailDW/demo.py
@@ -1,8 +1,23 @@
+from datetime import datetime
+from time import sleep
+
 import psycopg2
 from psycopg2.extensions import connection
 
-from RetailDW.etlutils import create_table, incremental_load
-from RetailDW.product import PRODUCT_TABLE, PRODUCT_CREATE_SQL_PG
+import mysql.connector
+from mysql.connector import connect
+
+from RetailDW.etlutils import (
+    create_table,
+    load_source_table,
+    extract_table_to_target,
+    ETL_HISTORY_CREATE_MYSQL,
+)
+from RetailDW.product import (
+    PRODUCT_TABLE,
+    PRODUCT_CREATE_SQL_PG,
+    PRODUCT_CREATE_SQL_MYSQL,
+)
 
 
 def demo1() -> None:
@@ -12,7 +27,7 @@ def demo1() -> None:
     inserts and 50 updates.
     """
     source_connection: connection = psycopg2.connect(
-        "dbname=postgres host=172.17.0.1 user=postgres password=postgres"
+        "dbname=retaildw host=172.17.0.1 user=user1 password=user1"
     )
 
     total_inserts = 0
@@ -20,14 +35,14 @@ def demo1() -> None:
 
     create_table(source_connection, PRODUCT_CREATE_SQL_PG)
 
-    inserted, updated = incremental_load(
+    inserted, updated = load_source_table(
         source_connection, PRODUCT_TABLE, n_inserts=5000, n_updates=0
     )
     total_inserts += inserted
     total_updates += updated
 
     for _ in range(5):
-        inserted, updated = incremental_load(
+        inserted, updated = load_source_table(
             source_connection, PRODUCT_TABLE, n_inserts=200, n_updates=50
         )
         total_inserts += inserted
@@ -36,3 +51,88 @@ def demo1() -> None:
     print(f"{total_inserts} inserts and {total_updates} updates processed.")
 
     source_connection.close()
+
+
+def demo2() -> None:
+    """Load Product records to source system and extract to target system. 
+
+    Create the product table and load 5000 rows to the source system.  Then run a loop of 5 incremental loads, each
+    having 200 inserts and 50 updates. After each incremental load, extract records from the source system to the 
+    target system. The extract only reads data updated since the prior extract.
+    
+    Insert and update counts are printed after each operation.
+    """
+    source_connection: connection = psycopg2.connect(
+        dbname="retaildw", host="172.17.0.1", user="user1", password="user1"
+    )
+    target_connection: connection = connect(
+        host="172.17.0.1",
+        user="user1",
+        password="user1",
+        database="retaildw",
+        charset="utf8",
+    )
+
+    total_inserts_source = 0
+    total_updates_source = 0
+    total_inserts_target = 0
+    total_updates_target = 0
+
+    timestamp = datetime.now()
+
+    create_table(source_connection, PRODUCT_CREATE_SQL_PG)
+    create_table(target_connection, PRODUCT_CREATE_SQL_MYSQL)
+    create_table(target_connection, ETL_HISTORY_CREATE_MYSQL)
+
+    inserted, updated = load_source_table(
+        source_connection,
+        PRODUCT_TABLE,
+        n_inserts=5000,
+        n_updates=0,
+        timestamp=timestamp,
+    )
+
+    print(f"{inserted} inserts and {updated} updates processed at source: {timestamp}.")
+
+    total_inserts_source += inserted
+    total_updates_source += updated
+
+    for _ in range(5):
+
+        sleep(3)
+        timestamp = datetime.now()
+        inserted, updated = load_source_table(
+            source_connection,
+            PRODUCT_TABLE,
+            n_inserts=200,
+            n_updates=50,
+            timestamp=timestamp,
+        )
+
+        print(
+            f"{inserted} inserts and {updated} updates processed at source: {timestamp}."
+        )
+
+        total_inserts_source += inserted
+        total_updates_source += updated
+
+        inserted, updated, from_time, to_time = extract_table_to_target(
+            source_connection, target_connection, PRODUCT_TABLE
+        )
+
+        print(
+            f"{inserted} inserts and {updated} updates processed at target: From: {from_time} To: {to_time} "
+        )
+
+        total_inserts_target += inserted
+        total_updates_target += updated
+
+    print(
+        f"{total_inserts_source} inserts and {total_updates_source} updates processed at source."
+    )
+    print(
+        f"{total_inserts_target} inserts and {total_updates_target} updates processed at target."
+    )
+
+    source_connection.close()
+    target_connection.close()
